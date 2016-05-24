da4c919352ff2178ad10ac3df92593902702c0f2
Leonardo <leoded94@hotmail.com>
2016-05-24 15:35:42 -0500

    Prueba caja negra

--- ingreso_pacientes/tests.py
+++ ingreso_pacientes/tests.py
@@ -5,10 +5,15 @@ from ingreso_pacientes.models import Paciente
 #ABABABABAB
 
 class CrearPaciente(TestCase):
-
+	def setUp(self):
+		self.Cliente = Client()
     def crear_paciente(self):
-        self.paciente = Consulta.objects.create(nombres='Juan',apellidos='Vargas',cedula='0955555555')
+        self.paciente = Paciente.objects.create(nombres='Juan',apellidos='Vargas',cedula='0955555555')
 
     def test_crear_paciente(self):
         self.crear_paciente()
         self.assertIsInstance(self.paciente,Paciente,'paciente creado exitosamente')
+
+	def test_crear_paciente_view(self):
+		respuesta = self.cliente.post('/pacientes/', ("nombres":"John","apellidos":"Connor","cedula":"0955555555"))
+		self.assertContains(respuesta, "guardado correctamente")
